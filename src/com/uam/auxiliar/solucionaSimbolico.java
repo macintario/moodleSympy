3f68e92892f7884ca2c825aba1b8cd607cd2a32a
macintario <macintario@gmail.com>
2019-11-11 18:02:44 -0600

    Look and feel de galois completo TODO: Referencias a los ejercicios correctos

--- src/com/uam/auxiliar/solucionaSimbolico.java
+++ src/com/uam/auxiliar/solucionaSimbolico.java
@@ -1,6 +1,7 @@
 package com.uam.auxiliar;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,11 +18,11 @@ public class solucionaSimbolico {
                     "f = parse_latex(r\" $EXPRESION$ \")\n" +
                     "fh=f.subs(x,x+h)\n" +
                     "drv=(fh-f)/h\n" +
-                    "salida.write(\"$$\\displaystyle f(x)=%s$$<br/><br/>\\n\" %(latex(f)))\n" +
-                    "salida.write(\"$$\\displaystyle f(x+h)=%s$$<br/><br/>\\n\" %latex(fh))\n" +
-                    "salida.write(\"$$\\displaystyle \\frac{f(x+h)-f(x)}{h}=%s=%s$$<br/><br/>\n\" % (latex(drv),latex(drv.simplify())))\n" +
+                    "salida.write(\"$$\\\\displaystyle f(x)=%s$$<br/><br/>\\n\" %(latex(f)))\n" +
+                    "salida.write(\"$$\\\\displaystyle f(x+h)=%s$$<br/><br/>\\n\" %latex(fh))\n" +
+                    "salida.write(\"$$\\\\displaystyle \\\\frac{f(x+h)-f(x)}{h}=%s=%s $$ <br/><br/>\" % (latex(drv),latex(drv.simplify())))\n" +
                     "drv=limit(drv,h,0)\n" +
-                    "salida.write(\"$$\\displaystyle f'(x)\\lim_{h\\to 0} \\frac{f(x+h)-f(x)}{h}=%s=%s$$<br/><br/>\\n\" % (latex(drv),latex(drv.simplify())))\n";
+                    "salida.write(\"$$\\\\displaystyle f'(x)=\\\\lim_{h\\\\to 0} \\\\frac{f(x+h)-f(x)}{h}=%s=%s$$<br/><br/>\\n\" % (latex(drv),latex(drv.simplify())))\n";
     public static String Incremento(String expresion) {
         String script = PLANTILLA_SYMPY;
         String solucion="";
@@ -29,7 +30,6 @@ public class solucionaSimbolico {
         try(FileWriter scriptFile = new FileWriter("/tmp/script.py");
             BufferedWriter bw = new BufferedWriter(scriptFile)){
             bw.write(script);
-
         }catch (IOException e){
             System.err.format("Escribiendo Script IOException: %s%n", e);
         }
@@ -45,6 +45,8 @@ public class solucionaSimbolico {
         }
         try {
             solucion = new String(Files.readAllBytes(Paths.get("/tmp/solucion.txt")));
+            //File f = new File("/tmp/solucion.txt");
+            //f.delete();
         }catch(java.io.IOException e){
             System.err.format("Leyendo Solucion IOException: %s%n", e);
         }
--- src/com/uam/constantes/Constantes.java
+++ src/com/uam/constantes/Constantes.java
@@ -22,29 +22,70 @@ public class Constantes {
             + "    </questiontext>\n"
             + "    <generalfeedback format=\"html\">\n"
             + "      <text><![CDATA["
-            + "<span style=\"color: #000000; font-size: large;\"><strong>Instrucciones para hacer autoevaluaciones y poder presentar examen de unidad.</strong></span>"
-            +"$$ a_2).$$ Nos interesa que usted domine el ejercicio aquí mostrado; para ello tendrá que seguir el procedimiento detallado que ofrece Symbolab. "
+            + "<span style=\"color: #000000; font-size: large;\"><strong>Instrucciones para hacer autoevaluaciones y poder presentar examen de unidad.</strong></span><br/><br/>"
+            + "$$ a_2).$$ Nos interesa que usted domine el ejercicio aquí mostrado; para ello tendrá que seguir el procedimiento detallado que ofrece Symbolab."
+            + " "
             +"(Cualquier duda con Symbolab, consulta a cualquier profesor de galoisenlinea, para ello acude a Sala SAV-01 en horarios de clase.) <br/>"
             +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$$ a_3).$$ Realice tantas autoevaluaciones como usted lo necesite."
             +"<br/><br/><span style=\"color: #000000; font-size: x-large\"><strong>SOLUCIÓN:</strong></span><br/>"
-            +"$SOLUCION$"
-            +"<span style=\"color: #000000; font-size: large\"><strong>PROCEDIMIENTO DE SOLUCION PASO A PASO</strong></span>"
+            +"<center>$SOLUCION$</center>"
+            +"<span style=\"color: #ff0000; font-size: large\"><strong>PROCEDIMIENTO DE SOLUCION PASO A PASO</strong></span>"
             +"<br/><br/>En este ejercicio y con objeto de que uses uno de los mejores Sistemas Algebraicos Computacionales (CAS) llamado "
-            +"<span style=\"color: #ff0000\"><strong>Symbolab</strong></span>, obtendrás el procedimiento de solución de este ejercicio paso a paso."
-+"Es importante que analice el procedimiento dado por Symbolab; en examen tendrás que hacer el procedimiento detallado de algún ejercicio similar."
-+"Si requieres revisión en algún punto de tu desarrollo en papel, muestra tu procedimiento detallado a algún profesor de galoisenlinea y con mucho gusto aclararemos tus dudas."
-+"Click en cada fórmula en azul y copia el texto:"
-+"La derivada de la función $$f(x)$$ es:"
-+"$EXPRESION$"
-+"Click en icono de Symbolab:     c. Pega en la ventana de entrada de datos de Symbolab tu texto copiado. Allí obtendrás la solución del ejercicio paso a paso."
-+"Biblioteca en galoisenlinea:"
-+"Detalles paso a paso para resolver el ejercicio del libro de Thomas en página y número de ejercicio: Thomas147-9, Click.... en imagen   c"
-+"LIBRO DE TEXTO Y SOLUCIONARIO AL LIBRO DE TEXTO:"
-+"TEXTO: Para teoría y ejercicios consulta el capítulo 3, página 102, 105, 115, 135 del libro: George B. Thomas, Jr. Decimosegunda edición Massachusetts Institute of Technology. c"
-+"SOLUCIONARIO AL TEXTO: Consulta el libro: THOMAS' CALCULUS, Solution Manual, Twelfth Edition. c"
-+"CAS (Sistemas algebraicos computacionales):"
-+"En las autoevaluaciones, una vez que realices cada ejercicio, te sugerimos usar cualquiera de los siguientes Sistemas Algebraicos Computacionales (CAS) para comprobar tus cálculos."
-+"En exámenes no podrás usar ninguno de estos CAS. Click en iconos para abrir CAS."
+            +"<span style=\"color: #ff0000\"><strong>Symbolab</strong></span>, obtendrás el procedimiento de solución de este ejercicio paso a paso.<br/>"
+            +"Es importante que analice el procedimiento dado por Symbolab; en examen tendrás que hacer el procedimiento detallado de algún ejercicio similar.<br/><br/>"
+            +"Si requieres revisión en algún punto de tu desarrollo en papel, muestra tu procedimiento detallado a algún profesor de galoisenlinea y "
+            +"con mucho gusto aclararemos tus dudas. <br/>"
+            +"<ul style=\"list-style-type:square\"><li>Click en cada fórmula en azul y copia el texto:<br><br>"
+            +"<center><span style=\"color:#ff0000; fontsize:medium;\"><strong>La derivada de la función $$f(x)$$ es:</strong></span><br/><br/>"
+            +"<span style=\"color: #0000ff; font-size:x-large;\"><strong> "
+            +"\\frac{d}{dx}\\left($EXPRESION$ \\right)"
+            + "</strong></span></center></li>"
+            +"<li>Click en icono de Symbolab:<a title=\"Symbolab\" href=\"https://www.symbolab.com/solver\" target=\"_blank\">"
+            +"<img title=\"Symbolab\" style=\"width: 100px; height: 70px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/propaganda/symbolab.jpg\" border=\"0\" height=\"40\" hspace=\"0\" width=\"40\"></a></li>"
+            +"<li>Pega en la ventana de entrada de datos de Symbolab tu texto copiado. Allí obtendrás la solución del ejercicio paso a paso.</li></ul>"
+            +"<span style=\"color: #ff0000; font-size:x-large; \"><strong>Biblioteca en galoisenlinea:</strong></span><br/><br/><br/>"
+            +"<center>\n" +
+            "<span style=\"color: #000000; font-size: x-large;\"><strong>\n" +
+            "Detalles paso a paso para resolver  el ejercicio del libro de Thomas en página y número de ejercicio: Thomas147-9, Click....  en imagen \n" +
+            "<a title=\" matematicas en galoisenlinea\" href=\" http://galois.azc.uam.mx/mate/INTEGRAL/thomas147-9.pdf\" target=\" _blank\"><img title=\" Desigualdad cuadratica\" style=\" width:50px;height:50px;\" alt=\" c\" src=\" http://galois.azc.uam.mx/mate/imagenes/pdf.png\" border=\" 0\" height=\" 50\" hspace=\" 0\" width=\" 50\"></a>&nbsp;\n" +
+            "\n" +
+            "</strong></span></center>"
+            +"<strong>\n" +
+            "\n" +
+            "\t\n" +
+            "<br>\n" +
+            "<br>\n" +
+            "<br>\n" +
+            "\n" +
+            "<span style=\"color: #ff0000; font-size: large;\"><strong>\n" +
+            "LIBRO DE TEXTO Y SOLUCIONARIO AL LIBRO DE TEXTO:</strong></span><br>\n" +
+            "\t<br>\n" +
+            "TEXTO: Para teoría y ejercicios consulta el capítulo 3, página 102, 105, 115, 135  del libro: George B. Thomas, Jr. Decimosegunda edición \n" +
+            "Massachusetts Institute of Technology.\n" +
+            "<a title=\"matematicas en galoisenlinea \" href=\"http://galois.azc.uam.mx/mate/diferencial/thomas.jpg\" target=\"_blank\"><img title=\"c\" style=\"width: 90px; height: 100px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/diferencial/thomas.jpg\" border=\"0\" height=\"50\" hspace=\"0\" width=\"50\"></a>\n" +
+            "<br><br>&nbsp;\n" +
+            "<br>\n" +
+            "SOLUCIONARIO AL TEXTO: Consulta el libro: THOMAS' CALCULUS, Solution Manual,  Twelfth Edition.\n" +
+            "<a title=\"matematicas en galoisenlinea \" href=\"http://galois.azc.uam.mx/mate/diferencial/solucionariothomas.jpg\" target=\"_blank\"><img title=\"c\" style=\"width: 90px; height: 100px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/diferencial/solucionariothomas.jpg\" border=\"0\" height=\"50\" hspace=\"0\" width=\"50\"></a>\n" +
+            "<br><br>&nbsp;\n" +
+            "<br>\n" +
+            "<br>\n" +
+            "<br>\n" +
+            "<span style=\"color: #ff0000; font-size: large;\"><strong>\n" +
+            "CAS (Sistemas algebraicos computacionales):</strong></span><br>\n" +
+            "\n" +
+            "En las autoevaluaciones, una vez que realices cada ejercicio, te sugerimos usar cualquiera de los \n" +
+            "siguientes Sistemas Algebraicos Computacionales (CAS) para comprobar tus cálculos. \n" +
+            "<br>\n" +
+            "En exámenes no podrás usar ninguno de estos CAS.   Click en iconos para abrir CAS.<br>&nbsp;\n" +
+            "<br>\n" +
+            "<a title=\"Symbolab\" href=\"https://www.symbolab.com/solver\" target=\"_blank\"><img title=\"Symbolab\" style=\"width: 120px; height: 45px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/propaganda/symbolab.jpg\" border=\"0\" height=\"40\" hspace=\"0\" width=\"40\"></a>\n" +
+            "&nbsp;&nbsp;\n" +
+            "<a title=\"Mathway\" href=\"https://www.mathway.com/\" target=\"_blank\"><img title=\"Mathway\" style=\"width: 150px; height: 45px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/imagenes/mathway.png\" border=\"0\" height=\"40\" hspace=\"0\" width=\"40\"></a>\n" +
+            "&nbsp;\n" +
+            "<a title=\"Wiris\" href=\"http://www.wiris.net/demo/wiris/manual/es/html/tour/index.html\" target=\"_blank\"><img title=\"Wiris\" style=\"width: 100px; height: 50px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/imagenes/wiris.jpg\" border=\"0\" height=\"40\" hspace=\"0\" width=\"40\"></a>\n" +
+            "&nbsp;\n" +
+            "<a title=\"Wolfram Alpha\" href=\"https://www.wolframalpha.com/\" target=\"_blank\"><img title=\"Wolfram Alpha\" style=\"width: 150px; height: 50px;\" alt=\"c\" src=\"http://galois.azc.uam.mx/mate/imagenes/wolframalpha.png\" border=\"0\" height=\"40\" hspace=\"0\" width=\"40\"></a><br><br>&nbsp;</strong>"
             + "    ]]></text>\n"
             + "    </generalfeedback>\n"
             + "    <penalty>0.3333333</penalty>\n"
